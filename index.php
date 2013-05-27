7ae1dc645fa4b8d1f14846f68f4967dd53bf4f7c
tq0fqeu <tq0fqeu@gmail.com>
2013-05-27 15:31:22 +0800

    Follow lp coding style

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 /* lp app root */
 // ↑____ for aoi . Do Not Delete it.
 /****  load lp framework  ***/
-define( 'DS' , DIRECTORY_SEPARATOR );
-define( 'AROOT' , dirname( __FILE__ ) . DS  );
+define('DS', DIRECTORY_SEPARATOR);
+define('AROOT', dirname(__FILE__) . DS);
 
 //ini_set('include_path', dirname( __FILE__ ) . DS .'_lp' ); 
-include_once( '_lp'.DS .'lp.init.php' );
+include_once('_lp' . DS . 'lp.init.php');
 /**** lp framework init finished ***/
